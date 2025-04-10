3e176375ceadb05f8425d82787571b9dc350f007
DRKS <CNH923@gmail.com>
2025-04-10 19:47:37 -0400

    minor button things

--- Assets/Scripts/View/MainMenu.cs
+++ Assets/Scripts/View/MainMenu.cs
@@ -7,17 +7,20 @@ public class MainMenu : MonoBehaviour
     public void StartGame()
     {
         // Load the game scene
+        //maybe add some effects?
         UnityEngine.SceneManagement.SceneManager.LoadScene("GameScene");
     }
 
     public void Options()
     {
         // Load the options menu
+        // no idea what to put here atm, probably sound control and deleting saves
     }
 
     public void ShareData()
     {
         // Share game data
+        // add the whole 
     }
 
     public void OpenCredits()
