de3c096d37b9c2d9318cac13ed41ee9664b2428f
DRKS <CNH923@gmail.com>
2025-04-21 22:05:29 -0400

    some bgm manager stuff, shit not working yet

--- Assets/Scripts/Presenter/MenuBGMManager.cs
+++ Assets/Scripts/Presenter/MenuBGMManager.cs
@@ -7,38 +7,59 @@ public class MenuBGMManager : MonoBehaviour, IDataPersistence
     [SerializeField] private AudioSource music;
     [SerializeField] private AudioSource soundEffects;
 
-    [SerializeField] private bool allowMusic = true;
+    [SerializeField] private AudioClip buttonClick;
+    [SerializeField] private AudioClip backgroundMusic;
+
+    public bool allowMusic = true;
     [SerializeField] private bool isMusicPlaying = true;
-    [SerializeField] private bool allowSfx = true;
+    public bool allowSfx = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        LoadData();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        SaveData();
+        if (allowMusic)
+        {
+            if (!isMusicPlaying)
+            {
+                music.Play();
+                isMusicPlaying = true;
+            }
+        }
+        else
+        {
+            if (isMusicPlaying)
+            {
+                music.Stop();
+                isMusicPlaying = false;
+            }
+        }
     }
 
     public void LoadData(GameData data)
     {
-        
+        allowMusic = data.allowMusic;
+        allowSfx = data.allowSfx;
     }
 
     public void SaveData(GameData data)
     {
-        
+        data.allowSfx = allowSfx;
+        data.allowMusic = allowMusic;
     }
 
-    void PlaySound(){
-
-    }
-
-    void PlayMusic(){
-        
+    void PlaySound()
+    {
+        if (allowSfx)
+        {
+            soundEffects.Play();
+        }
     }
 
 }
